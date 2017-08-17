94c1c746cc9418c43f295d7048b993fef4580ea8
AkemiHomura <loveforyoshino@qq.com>
2017-08-17 09:50:44 +0800

    test

---
+++ application/controllers/Upload.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Upload extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('upload');
+    }
+
+    public function uploadAction()
+    {
+        $jsonData = $this->input->post('images');
+        $imagesdata = json_decode($jsonData);
+        
+        //路径
+        $upload_path = './uploads/'.date('Y-m-d',time());
+        if (!file_exists($upload_path)) {
+            mkdir($upload_path,0777,true);
+        }
+
+        foreach ($imagesdata as $key => $value) {
+
+            if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $value, $result)){  //匹配图片格式
+                $type = $result[2];
+                $data = str_replace($result[1], '', $value);
+                $new_file = $upload_path.'/'.md5($value).".{$type}";
+                file_put_contents($new_file, base64_decode(str_replace(' ', '+',$data)));
+            }
+        }
+
+    }
+}
